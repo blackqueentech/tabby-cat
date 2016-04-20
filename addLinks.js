735cc032831d5614719274ef6fb7ddcc2b8388ff
Della Anjeh <dellaanjeh@Dellas-MacBook-Pro.local>
2016-04-20 12:17:13 -0700

    fixes add link functionality

--- addLinks.js
+++ addLinks.js
@@ -1,16 +1,18 @@
-window.onload = function() {
+//window.onload = function() {
 var counter = 3;
 var limit = 15;
 
-function addLinks(divName){
-    if (counter == limit) {
-	alert("Tabby Cat says that's enough links, yeesh. 15 is enough.");
-    }
-    else {
-	var newdiv = document.createElement('div');
-	newdiv.innerHTML = "Link " + (counter + 1) + " <br><input type='text' class='form-control' name='sessionLinks[]'><br/>";
-	document.getElementById(divName).appendChild(newdiv);
-	counter++;
-    }
-}
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('addLink');
+    link.addEventListener('click', function() {
+	    if (counter == limit) {
+		alert("Tabby Cat says that's enough links, yeesh. 15 is enough.");
+	    } else {
+		var newdiv = document.createElement('div');
+		newdiv.innerHTML = "Link " + (counter + 1) + " <br><input type='text' class='form-control' name='sessionLinks[]'><br/>";
+		document.getElementById('linkList').appendChild(newdiv);
+		counter++;
+	    }
+	});
+    });
+//}
